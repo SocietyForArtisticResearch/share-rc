0418c655b0a4f5b31793b8b763191328daa48304
Luc Döbereiner <luc@computer.localdomain>
2018-07-13 16:18:39 +0200

    with http server

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-//var http = require('http');
-//var express = require('express');
+var http = require('http');
+var express = require('express');
 var ShareDB = require('sharedb');
 var WebSocket = require('ws');
 var WebSocketJSONStream = require('websocket-json-stream');
@@ -23,18 +23,18 @@ function createDoc(callback) {
 
 function startServer() {
   // Create a web server to serve files and listen to WebSocket connections
-//  var app = express();
+  var app = express();
 //  app.use(express.static('static'));
-//  var server = http.createServer(app);
+  var server = http.createServer(app);
 
   // Connect any incoming WebSocket connection to ShareDB
-    //    var wss = new WebSocket.Server({server: server});
-    var wss = new WebSocket('ws://dev.researchcatalogue.net/share');
+    var wss = new WebSocket.Server({server: server});
+//    var wss = new WebSocket('ws://dev.researchcatalogue.net/share');
     wss.on('connection', function(ws, req) {
 	var stream = new WebSocketJSONStream(ws);
 	backend.listen(stream);
     });
 
-//  server.listen(8080);
+    server.listen(8080);
   //console.log('Listening on http://localhost:8080');
 }
